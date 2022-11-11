e53c918f624382a05bd0eb3f56dccc52b69483b3
Matthew Harrison Laptop <matthew_s_harrison@yahoo.com>
2022-11-12 05:32:31 +0700

    third commit

--- sanity.ts
+++ sanity.ts
@@ -3,7 +3,7 @@ import createImageUrlBuider from '@sanity/image-url'
 
 export const config = {
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+    projectId: 'h01gsdth',
     apiVersion: '2021-03-25',
     useCdn: process.env.NODE_ENV === 'production'
 }
